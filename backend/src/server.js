d47629748b9fb73dccf7f68002c6ebaa8724dc4a
hpaanonlineuniversity <hpaanonlineuniversity@gmail.com>
2025-10-20 08:18:27 +0630

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = new express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+          console.log(`✅ Server is running on http://0.0.0.0:${PORT}`);
+          console.log(`✅ Accessible via http://localhost:${PORT}`);
+      }); 
