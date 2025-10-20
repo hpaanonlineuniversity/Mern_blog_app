36cc26b39d6ea46be444d921878cf99bbd0f1c0c
hpaanonlineuniversity <hpaanonlineuniversity@gmail.com>
2025-10-20 13:50:25 +0630

    backend connect mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import connectDB from './configs/db.js';
 
 const app = new express();
 const PORT = process.env.PORT || 3000;
@@ -7,7 +8,13 @@ app.get('/', (req, res) => {
     res.send('Hello World!123');
 });
 
-app.listen(PORT, '0.0.0.0', () => {
+connectDB().then(() => {
+        // '0.0.0.0' ကို explicitly bind လုပ်ပါ
+      app.listen(PORT, '0.0.0.0', () => {
           console.log(`✅ Server is running on http://0.0.0.0:${PORT}`);
           console.log(`✅ Accessible via http://localhost:${PORT}`);
       }); 
+}).catch((err) => {
+  console.error('Database connection failed:', err);
+  process.exit(1); // Exit process with failure
+});
