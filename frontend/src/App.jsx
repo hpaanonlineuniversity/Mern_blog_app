892f9d217a631e102163863d04ab1d1d07fe8489
hpaanonlineuniversity <hpaanonlineuniversity@gmail.com>
2025-10-21 14:27:25 +0630

    remove public profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,6 @@ import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
 import Header from './components/Header';
 import PrivateProfile from './pages/PrivateProfile';
-import PublicProfile from './pages/PublicProfile';
 import FooterComponent from './components/FooterComponent';
 import Callback from './pages/Callback';
 
@@ -20,8 +19,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/about' element={<About />} />
-          <Route path='/profile' element={<PublicProfile />} />
-          <Route path='/private-profile' element={<PrivateProfile />} />
+          <Route path='/profile' element={<PrivateProfile />} />
           <Route path='/sign-in' element={<Signin />} />
           <Route path='/sign-up' element={<Signup />} />
           <Route path='/dashboard' element={<Dashboard />} />
