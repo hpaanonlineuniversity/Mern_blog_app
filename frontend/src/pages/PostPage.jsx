2ddf2181c66d79091092def8549247025a6690e6
hpaanonlineuniversity <hpaanonlineuniversity@gmail.com>
2025-10-24 10:14:11 +0630

    add comment section

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -2,7 +2,7 @@ import { Button, Spinner } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router';
 import CallToAction from '../components/CallToAction';
-// import CommentSection from '../components/CommentSection';
+import CommentSection from '../components/CommentSection';
 import PostCard from '../components/PostCard';
 
 export default function PostPage() {
@@ -188,16 +188,7 @@ export default function PostPage() {
       {/* Comment Section */}
       <section className='max-w-4xl mx-auto w-full mt-12 px-4'>
         <div className='border-t border-gray-200 pt-8'>
-          <h3 className='text-2xl font-bold text-gray-800 mb-6 text-center'>Comments</h3>
-          <div className='bg-gray-50 p-8 rounded-xl text-center border border-gray-200'>
-            <svg className="w-12 h-12 text-gray-400 mx-auto mb-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
-            </svg>
-            <p className='text-gray-500 text-lg mb-4'>Comment section coming soon...</p>
-            <Button color='gray' className='px-6' disabled>
-              Add Comment
-            </Button>
-          </div>
+          <CommentSection postId={post._id} />
         </div>
       </section>
 
